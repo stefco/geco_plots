f2a42cc5699cc086cd5ec6adaf00a35f1d996431
vagrant <vagrant@vagrant.vm>
2016-04-06 19:46:28 +0000

    Made fixes to the script so it would run. Added ability to input dates and times from the command line.

--- make_timediff_plot.py
+++ make_timediff_plot.py
@@ -9,16 +9,16 @@ import matplotlib.pyplot as plt
 # exit if no arguments
 if len(sys.argv) == 1:
     print 'Not enough arguments.'
-    print 'For a input format type \'make_plot.py\' -h'
+    print 'For a input format type \'make_timediff_plot.py -h\''
     exit(1)
 
 if sys.argv[1] == '-h':
-    print 'python make_timediff_plot.py \'[start date]\' \'[end date]\''
+    print 'python make_timediff_plot.py \'start date\' \'end date\''
     print 'Enter dates as Month Day, Year hh:mm:ss'
     exit()
 else:
-    start_time = sys.argv[1]
-    end_time = sys.argv[2]
+    start = sys.argv[1]
+    end = sys.argv[2]
 
 # remove the first 6 lines and the string "Data: "
 def remove_header_and_text(string):
@@ -46,38 +46,48 @@ def tconvert(tstr=""):
     return int(os.popen("lalapps_tconvert " + tstr).read())
 
 # make list of times that correspond to the necessary frame files
-def make_times_list(start_time , end_time):
+def make_times_list(s_time , e_time):
     print('making list of times')
-    startn = int(tconvert(start_time) / 64)
-    if end % 64 == 0:
-        endn = ((tconvert(end_time) / 64) - 1)
+    gps_start = tconvert(s_time)
+    gps_end = tconvert(e_time)
+    startn = int(gps_start) / 64
+    if int(gps_end) % 64 == 0:
+        endn = (gps_end / 64) - 1
     else:
-        endn = int(tconvert(end_time) / 64)
+        endn = int(gps_end) / 64
     return [64 * i for i in range(startn, endn + 1)]
+    print 'made a times array'
 
 def make_plot(x_axis, y_axis):
     print('making plots')
     plt.plot(x_axis, y_axis)
     plt.xlabel('GPS Time')
     plt.ylabel('Offset')
-    plt.title('Time difference from ' + start_time + ' until ' + end_time)
-    plt.savefig('Time difference from ' + start_time + 'until ' + end_time + '.png')
+    plt.title('Time difference from ' + start + ' until ' + end)
+    plt.savefig('Time difference from ' + start + ' until ' + end + '.png')
 
 def main(start_time, end_time):
-    x = []
-    t = []
+    x = [ ] 
+    t = [ ]
     for time in make_times_list(start_time, end_time):
-        path = time + ".dat"
-        if os.path.exists(path):
+        path = str(time) + ".dat"
+        if int(time) % 40000 == 0:
+            print 'Just checking in ' + str(time)
             # set up the processes for acquiring and processing the data
-            print('loading file')
+            with open(path,'r') as infile:
+                data_string = remove_header_and_text(infile.read())
+            # append arrays to existing timeseries and times
+            x += [float(x) for x in data_string.split(',')]
+            t += [time + dt/16 for dt in range(1024)]
+        elif os.path.exists(path):
+            # set up the processes for acquiring and processing the data
             with open(path,'r') as infile:
                 data_string = remove_header_and_text(infile.read())
             # append arrays to existing timeseries and times
             x += [float(x) for x in data_string.split(',')]
             t += [time + dt/16 for dt in range(1024)]
         else:
-            print('file' + path + 'not found.')
-    make_plot(x, t)
+            print('file ' + path + ' not found.')
+    make_plot(t, x)
 
-main(start_time, end_time)
+main(start, end)
