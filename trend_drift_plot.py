942b35a57dfba55785d170ad63979cc9a19c6570
vagrant <vagrant@vagrant.vm>
2016-05-30 18:22:43 +0000

    Takes stdin in the form output by  geco_trend_dump and creates a plot from those data points

---
+++ trend_drift_plot.py
@@ -0,0 +1,83 @@
+#_!/usr/bin/env python
+
+import os
+import subprocess
+import math
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+import fileinput
+import argparse
+
+micros_per_second = 1000000
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-l', help='the location of the trend data being used')
+parser.add_argument('-t')
+args = parser.parse_args()
+location = vars(args)['l']
+if vars(args)['t'] == None:
+    trend = 'Mean'
+else:
+    trend = vars(args)['t']
+
+#Takes input from geco_trend_dump and makes an array of times and corresponding
+#trend values for that time
+def make_time_series():
+    times = []
+    trend = []
+    for line in sys.stdin:
+        #reads each line of the stdin from geco_trend_dump. for each line, the
+        #try, except loop makes sure the line contains only numbers. It then
+        #checks that each line has 2 entries, and that the data does not look
+        #anonymous
+        try:
+            values = [float(x) for x in line.split('\t')]
+            if len(values) == 2:
+                if values[1] == 0:
+                    print 'It seems that the signal was off during some part of ' + str(i[0])
+                elif abs(values[1]) * micros_per_second < .01:
+                    print 'The data at ' + str(values[0]) + ' is anomalously small.'
+                elif abs(values[1]) * micros_per_second > 4:
+                    print 'The data at ' + str(values[0]) + ' is anomalously large.'
+                else:
+                    times.append(values[0])
+                    trend.append(values[1])
+            else:
+                print line
+        except ValueError:
+            pass
+    return (times, trend)
+
+def make_plot(x_axis, y_axis):
+    #converts y_axis time to microseconds from seconds
+    y_axis = [i * micros_per_second for i in y_axis]
+    #creates array with slope and y-intercept of line of best fit
+    lobf_array = np.polyfit(x_axis, y_axis, 1)
+    #dimensionless quantity that characterizes drift
+    drift_coef = lobf_array[0]/micros_per_second
+    y_axis_lobf = np.poly1d(lobf_array)(x_axis)
+    tmp = [lobf_array[0] * i for i in x_axis]
+    tmp += lobf_array[1]
+    tmp -= y_axis
+    y_dif = tmp
+    print 'making plots'
+    fig = plt.figure()
+    plt.suptitle('WHATSHOULD THE TITLE OF THIS BE?')
+    ax1 = fig.add_subplot(211)
+    ax1.plot(x_axis, y_axis, '#ff0000')
+    ax1.plot(x_axis, y_axis_lobf, '#617d8d')
+    ax1.set_xlabel('GPS time')
+    ax1.set_ylabel('Offset [$\mu$s]')
+    ax2 = fig.add_subplot(212)
+    n, bins, patches = plt.hist(y_dif, 20, facecolor = '#139a0e')
+    ax2.set_xlabel('$\Delta$Time difference [$\mu$s]')
+    print drift_coef
+    fig.savefig('FILENAME.png')
+
+def main():
+    values = make_time_series()
+    make_plot(values[0], values[1])
+
+main()
