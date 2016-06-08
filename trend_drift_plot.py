869a863dc1ef1995a20737b2bdca2255778d648b
vagrant <vagrant@vagrant.vm>
2016-06-08 02:01:59 +0000

    Almost finished plotting software. Need to add titles and fix colors on the graphs.

--- trend_drift_plot.py
+++ trend_drift_plot.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 
+fitted_function_description = 'lobf'
 valid_trend_extensions = ['min', 'max', 'mean', 'n', 'rms']
 micros_per_second = 1000000
 
@@ -20,13 +21,10 @@ parser.add_argument('-f', help='Allows you to specify whether to use the line'\
     +' and the latter for the GPS clock.')
 args = parser.parse_args()
 location = vars(args)['l']
-fitted_function_description = 'lobf'
 if vars(args)['t'] == None:
-    trend = 'Mean'
+    trend = 'mean'
 else:
     trend = vars(args)['t']
-if vars(args)['f'] == 'constant' or vars(args)['f'] == 'lobf':
-    fitted_function_description = vars(args)['f']
 
 #Takes input from geco_trend_dump and makes an array of times and corresponding
 #trend values for that time
@@ -42,9 +40,8 @@ def make_time_series():
         #try, except loop makes sure the line contains only numbers. It then
         #checks that each line has 2 entries, and that the data does not look
         #anonymous
-        if line in valid_trend_extensions:
-            current_stat = statistics[line]
-            print current_stat
+        if line.replace("\n","") in valid_trend_extensions:
+            current_stat = statistics[line.replace("\n","")]
         try:
             values = [float(x) for x in line.split('\t')]
             if len(values) == 2:
@@ -69,11 +66,13 @@ def make_time_series():
 
 def make_plot(statistics_dictionary):
     stat = statistics_dictionary
+    fitted_function_description = vars(args)['f']
     #converts all trend data from seconds to microseconds
     for extension in valid_trend_extensions:
         stat[extension]['trend'] = [i * micros_per_second for i in\
             stat[extension]['trend']]
     if fitted_function_description == 'lobf':
+        fitted_function_description = 'line of best fit'
         #creates array with slope and y-intercept of line of best fit of the mean
         lobf_array = np.polyfit(stat['mean']['times'], stat['mean']['trend'], 1)
         #dimensionless quantity that characterizes drift
@@ -93,41 +92,67 @@ def make_plot(statistics_dictionary):
         tmp -= stat['min']['trend']
         min_dif = tmp
     elif fitted_function_description == 'constant':
+        fitted_function_description = 'average value of mean trend'
         drift_coef = 0
         average_value = sum(stat['mean']['trend'])/float(len(stat['mean']['trend']))
         fitted_function = np.poly1d(average_value)(stat['mean']['trend'])
         mean_dif = np.subtract(stat['mean']['trend'],fitted_function)
         max_dif = np.subtract(stat['max']['trend'],fitted_function)
         min_dif = np.subtract(stat['min']['trend'],fitted_function)
-    else:
-        print('you must pick either "lobf" or "constant" with the "-f" flag')
-        exit()
     mean_std_dev_array = [i-np.mean(mean_dif) for i in mean_dif]/np.std(mean_dif)
+    ##else:
+        ##print('You must specify either "lobf" or "constant" with with "-f" flag')
     print 'making plots'
-    fig = plt.figure(figsize=(6.5,9))
+    fig = plt.figure(figsize=(13,18))
     plt.suptitle('Characterization of diagnostic timing system 1PPS system, '\
                  + str(location))
     plt.subplots_adjust(top=0.88888888, bottom=0.1)
-    ax1 = fig.add_subplot(311)
-    ax1.set_title('Line of best fit versus offset')
+    ax1 = plt.subplot2grid((5,2),(0,0), colspan=2)
+    ax1.set_title('Line of best fit versus mean offset')
     ax1.plot(stat['mean']['times'], stat['mean']['trend'], '#ff0000')
     ax1.plot(stat['mean']['times'], fitted_function, '#617d8d')
-    ax1.fill_between(stat['mean']['times'], min_dif, max_dif, alpha = 0.5)
+    ax1.fill_between(stat['max']['times'], stat['max']['trend'] ,stat['min']\
+        ['trend'] , alpha = 0.5)
     ax1.set_xlabel('GPS time')
     ax1.set_ylabel('Offset [$\mu$s]')
-    ax3 = fig.add_subplot(313)
+    ax3 = plt.subplot2grid((5,2), (2,1))
     n, bins, patches = plt.hist(mean_dif, 20, facecolor = '#139a0e')
     ax3.set_xlabel('$\Delta$t [$\mu$s]')
     ax3.set_ylabel('Frequency')
     ax3.set_title('histogram of the residual')
-    ax2 = fig.add_subplot(312)
+    ax2 = plt.subplot2grid((5,2), (2,0))
     ax2.plot(stat['mean']['times'], mean_dif)
     ax2.set_xlabel('GPS time [s]')
-    ax2.set_title('Residual of the line of best fit')
+    ax2.set_title('Residual of the line of best fit and the mean')
     ax2.set_ylabel('Difference [$\mu$s]')
+    ax4 = plt.subplot2grid((5,2),(1,1))
+    ax4.plot(stat['max']['times'], max_dif, label='Line of best fit versus max offset')
+    ax4.plot(stat['min']['times'], min_dif, label='Line of best fit versus min offset')
+    ax4.legend(loc='best', fancybox=True, framealpha=0.5)
+    ax4.set_xlabel('GPS time')
+    ax5 = plt.subplot2grid((5,2),(1,0))
+    ax5.plot(stat['mean']['times'], stat['mean']['trend'], label = '')
+    ax5.legend(loc='best', fancybox=True, framealpha=0.5)
+    ax5.set_xlabel('GPS time [s]')
+    ax5.set_ylabel('Offset [$\mu$s]')
+    ax6 = plt.subplot2grid((5,2), (3,0))
+    n, bins, patches = plt.hist(min_dif, 20)
+    ax6.set_xlabel('$\Delta$t [$\mu$s]')
+    ax6.set_ylabel('Frequency')
+    ax6.set_title('Histogram of the residual the line of best fit and min')
+    ax7 = plt.subplot2grid((5,2), (3,1))
+    n, bins, patches = plt.hist(max_dif, 20)
+    ax7.set_xlabel('$\Delta$t [$\mu$s]')
+    ax7.set_ylabel('Frequency')
+    ax7.set_title('Histogram of the residual from the line of best fit and max')
+    ax8 = plt.subplot2grid((5,2), (4,0))
+    ax8.plot(stat['mean']['times'], mean_std_dev_array)
+    ax9 = plt.subplot2grid((5,2), (4,1))
+    n, bins, patches = plt.hist(max_dif, 20)
+    ax9.set_title('Histogram of standard deviations')
     print drift_coef
     plt.tight_layout(pad=0.4, w_pad=0.5, h_pad=1)
-    plt.subplots_adjust(left=0.2, right=0.8, top=0.9, bottom=0.1)
+    plt.subplots_adjust(left=0.1, right=0.95, top=0.95, bottom=0.05)
     fig.savefig('FILENAME.png')
 
 def main():
